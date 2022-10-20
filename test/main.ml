9a7f488a2614ba6b432508522d83201908b497ed
Ben Nozik <bhn9@cornell.edu>
2022-10-20 15:23:54 -0400

    Resolve merge conflict

--- test/main.ml
+++ test/main.ml
@@ -5,73 +5,51 @@ type state =
   | Dead
   | Alive
 
-type gameboard = state list list
+type gameboard = Gameboard.gameboard
 
-(*some starter patterns for the board
+(*some starter patterns for the board TODO: WOULD LIKE TO MAKE HELPER FUNCTION
+  THAT ALLOWS ONE TO CREATE BOARDS BY ONLY SPECIFIYING WHICH CELLS ARE ALIVE,
+  RATHER THAN WRITING IT ALL OUT, YA KNOW*)
 
-  TODO: WOULD LIKE TO MAKE HELPER FUNCTION THAT ALLOWS ONE TO CREATE BOARDS BY
-  ONLY SPECIFIYING WHICH CELLS ARE ALIVE, RATHER THAN WRITING IT ALL OUT, YA
-  KNOW*)
-
-let assert_equal_boards (gb1 : gameboard) (gb2 : gameboard) =
+let assert_equal_boards gb1 gb2 =
   if List.flatten gb1 = List.flatten gb2 then true else false
 
-let dead : gameboard =
-  [
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-  ]
-
-let block : gameboard =
-  [
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Alive; Alive; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Alive; Alive; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-  ]
-
-let toad =
-  [
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Alive; Alive; Alive; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Alive; Alive; Alive; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-  ]
-
-let glider =
-  [
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Alive; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Alive; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Alive; Alive; Alive; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-    [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
-  ]
-
+(* let dead : Gameboard.gameboard = [ [ Dead; Dead; Dead; Dead; Dead; Dead;
+   Dead; Dead; Dead; Dead ]; [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead;
+   Dead; Dead ]; [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
+   [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ]; [ Dead; Dead;
+   Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ]; [ Dead; Dead; Dead; Dead;
+   Dead; Dead; Dead; Dead; Dead; Dead ]; [ Dead; Dead; Dead; Dead; Dead; Dead;
+   Dead; Dead; Dead; Dead ]; [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead;
+   Dead; Dead ]; [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
+   [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ]; ] let block =
+   [ [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ]; [ Dead;
+   Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ]; [ Dead; Dead; Dead;
+   Dead; Dead; Dead; Dead; Dead; Dead; Dead ]; [ Dead; Dead; Dead; Alive; Alive;
+   Dead; Dead; Dead; Dead; Dead ]; [ Dead; Dead; Dead; Alive; Alive; Dead; Dead;
+   Dead; Dead; Dead ]; [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead;
+   Dead ]; [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ]; [
+   Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ]; [ Dead; Dead;
+   Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ]; [ Dead; Dead; Dead; Dead;
+   Dead; Dead; Dead; Dead; Dead; Dead ]; ] let toad = [ [ Dead; Dead; Dead;
+   Dead; Dead; Dead; Dead; Dead; Dead; Dead ]; [ Dead; Dead; Dead; Dead; Dead;
+   Dead; Dead; Dead; Dead; Dead ]; [ Dead; Dead; Dead; Dead; Dead; Dead; Dead;
+   Dead; Dead; Dead ]; [ Dead; Dead; Dead; Dead; Alive; Alive; Alive; Dead;
+   Dead; Dead ]; [ Dead; Dead; Dead; Alive; Alive; Alive; Dead; Dead; Dead; Dead
+   ]; [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ]; [ Dead;
+   Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ]; [ Dead; Dead; Dead;
+   Dead; Dead; Dead; Dead; Dead; Dead; Dead ]; [ Dead; Dead; Dead; Dead; Dead;
+   Dead; Dead; Dead; Dead; Dead ]; [ Dead; Dead; Dead; Dead; Dead; Dead; Dead;
+   Dead; Dead; Dead ]; ] let glider = [ [ Dead; Dead; Dead; Dead; Dead; Dead;
+   Dead; Dead; Dead; Dead ]; [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead;
+   Dead; Dead ]; [ Dead; Dead; Dead; Dead; Alive; Dead; Dead; Dead; Dead; Dead
+   ]; [ Dead; Dead; Dead; Dead; Dead; Alive; Dead; Dead; Dead; Dead ]; [ Dead;
+   Dead; Dead; Alive; Alive; Alive; Dead; Dead; Dead; Dead ]; [ Dead; Dead;
+   Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ]; [ Dead; Dead; Dead; Dead;
+   Dead; Dead; Dead; Dead; Dead; Dead ]; [ Dead; Dead; Dead; Dead; Dead; Dead;
+   Dead; Dead; Dead; Dead ]; [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead;
+   Dead; Dead ]; [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead ];
+   ] *)
 (* test functions to test functionality of gamboard.ml*)
 let init_gameboard_test name in_gb exp_out =
   name >:: fun _ -> assert_equal exp_out (init_gameboard in_gb)
@@ -92,11 +70,6 @@ let update_board_test name in_gb exp_out =
   name >:: fun _ -> assert_equal exp_out (update_board in_gb)
 
 (* test suite to execute gameboard tests*)
-let gameboard_tests =
-  [
-    init_gameboard_test "test empty board creates" None
-      dead (*TODO: implement more test cases*);
-  ]
-
-let suite = "test suite for CA" >::: List.flatten [ gameboard_tests ]
+let gameboard_tests = [ (*TODO: implement test cases*) ]
+let suite = "test suite for CA" >::: List.flatten []
 let _ = run_test_tt_main suite
