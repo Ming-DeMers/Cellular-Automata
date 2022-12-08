8455ee8709d490abe5d1b98d1c6e3e3fcb86833f
bhn9 <bhn9@cornell.edu>
2022-12-08 15:32:13 -0500

    Add some test cases for neighbors function

--- test/main.ml
+++ test/main.ml
@@ -6,7 +6,51 @@ module GoL = Make (B3_S23)
 
 (************** Tests for Standard Game of Life with Wraparound **************)
 let empty_10x10 = GoL.init_empty 10 10
-let b1_10x10 = GoL.init_empty 10 10
+
+let b55_10x10 =
+  let x = GoL.init_empty 10 10 in
+  GoL.birth_node x 5 5;
+  x
+
+let b00_10x10 =
+  let x = GoL.init_empty 10 10 in
+  GoL.birth_node x 0 0;
+  x
+
+let b09_10x10 =
+  let x = GoL.init_empty 10 10 in
+  GoL.birth_node x 0 9;
+  x
+
+let b90_10x10 =
+  let x = GoL.init_empty 10 10 in
+  GoL.birth_node x 9 0;
+  x
+
+let b99_10x10 =
+  let x = GoL.init_empty 10 10 in
+  GoL.birth_node x 9 0;
+  x
+
+let b05_10x10 =
+  let x = GoL.init_empty 10 10 in
+  GoL.birth_node x 0 5;
+  x
+
+let b95_10x10 =
+  let x = GoL.init_empty 10 10 in
+  GoL.birth_node x 9 5;
+  x
+
+let b50_10x10 =
+  let x = GoL.init_empty 10 10 in
+  GoL.birth_node x 5 0;
+  x
+
+let b59_10x10 =
+  let x = GoL.init_empty 10 10 in
+  GoL.birth_node x 5 0;
+  x
 
 let assert_equal_boards gb1 gb2 =
   let gb1_lst =
@@ -34,7 +78,45 @@ let loop_test name in_gb in_int exp_out =
   name >:: fun _ -> assert_equal exp_out (GoL.loop in_gb in_int)
 
 let neighbors_tests =
-  [ neighbors_test "neighbors of empty @ 0,0" empty_10x10 0 0 0 ]
+  [
+    (* empty board *)
+    neighbors_test "neighbors of empty @ 0,0" empty_10x10 0 0 0;
+    neighbors_test "neighbors of empty @ 9,9" empty_10x10 9 9 0;
+    (* only alive node at 5,5 *)
+    neighbors_test "neighbors of 5,5 @ 5,5" b55_10x10 5 5 0;
+    neighbors_test "neighbors of 5,5 @ 4,4" b55_10x10 4 4 1;
+    neighbors_test "neighbors of 5,5 @ 5,4" b55_10x10 5 4 1;
+    neighbors_test "neighbors of 5,5 @ 6,4" b55_10x10 6 4 1;
+    neighbors_test "neighbors of 5,5 @ 4,5" b55_10x10 4 5 1;
+    neighbors_test "neighbors of 5,5 @ 6,5" b55_10x10 6 5 1;
+    neighbors_test "neighbors of 5,5 @ 4,6" b55_10x10 4 6 1;
+    neighbors_test "neighbors of 5,5 @ 5,6" b55_10x10 5 6 1;
+    neighbors_test "neighbors of 5,5 @ 6,6" b55_10x10 6 6 1;
+    neighbors_test "neighbors of 5,5 @ 5,7" b55_10x10 5 7 0;
+    (* mid edge cases*)
+    neighbors_test "neighbors of 5,0 @ 4,9" b50_10x10 4 9 1;
+    neighbors_test "neighbors of 5,0 @ 5,9" b50_10x10 5 9 1;
+    neighbors_test "neighbors of 5,0 @ 6,9" b50_10x10 6 9 1;
+    neighbors_test "neighbors of 9,5 @ 0,4" b95_10x10 0 4 1;
+    neighbors_test "neighbors of 9,5 @ 0,5" b95_10x10 0 5 1;
+    neighbors_test "neighbors of 9,5 @ 0,6" b95_10x10 0 6 1;
+    neighbors_test "neighbors of 5,9 @ 4,0" b59_10x10 4 0 1;
+    neighbors_test "neighbors of 5,9 @ 5,0" b59_10x10 5 0 1;
+    neighbors_test "neighbors of 5,9 @ 6,0" b59_10x10 6 0 1;
+    neighbors_test "neighbors of 0,5 @ 9,4" b05_10x10 9 4 1;
+    neighbors_test "neighbors of 0,5 @ 9,5" b05_10x10 9 5 1;
+    neighbors_test "neighbors of 0,5 @ 9,6" b05_10x10 9 6 1;
+    (* corner cases *)
+    neighbors_test "neighbors of 0,0 @ 9,0" b00_10x10 9 0 1;
+    neighbors_test "neighbors of 0,0 @ 0,9" b00_10x10 0 9 1;
+    neighbors_test "neighbors of 0,0 @ 9,9" b00_10x10 9 9 1;
+    neighbors_test "neighbors of 9,0 @ 0,0" b90_10x10 0 0 1;
+    neighbors_test "neighbors of 9,0 @ 0,9" b90_10x10 0 9 1;
+    neighbors_test "neighbors of 9,0 @ 9,9" b90_10x10 9 9 1;
+    neighbors_test "neighbors of 0,9 @ 0,0" b09_10x10 0 0 1;
+    neighbors_test "neighbors of 0,9 @ 9,9" b09_10x10 9 9 1;
+    neighbors_test "neighbors of 0,9 @ 0,0" b09_10x10 9 0 1;
+  ]
 
 let gol_tests = List.flatten [ neighbors_tests ]
 
