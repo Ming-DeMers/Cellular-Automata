ea6f8707add3c8440366caff851a75e16f621145
Ming DeMers <mtd64@cornell.edu>
2022-12-14 00:53:48 -0500

    added test cases for one.

--- _build/default/gui/.gui.eobjs/gui.ml.d
+++
@@ -1,1 +0,0 @@
-gui/gui.ml: Array B3_S23 Cellular_automata Graphics MakeBoard Unix
--- _build/default/test/main.ml
+++ _build/default/test/main.ml
@@ -21,11 +21,6 @@ let one_tests =
   [
     make_n_test "make_n [1] 2 is [0; 1]" [ 1 ] 2 [ 0; 1 ];
     make_n_test "make_n 1;3 4 is [0; 0; 1; 3]" [ 1; 3 ] 4 [ 0; 0; 1; 3 ];
-    int_to_binary_test "int_to_binary 0 is [0]" 0 [ 0 ];
-    int_to_binary_test "int_to_binary 1 is [1]" 1 [ 1 ];
-    int_to_binary_test "int_to_binary 2 is [1; 0]" 2 [ 1; 0 ];
-    int_to_binary_test "int_to_binary 3 is [1; 1]" 3 [ 1; 1 ];
-    int_to_binary_test "int_to_binary 9 is [1; 0; 0; 1]" 9 [ 1; 0; 0; 1 ];
     binary_to_int_test "binary_to_int [0] is 0" [ 0 ] 0;
     binary_to_int_test "binary_to_int [1] is 1" [ 1 ] 1;
     binary_to_int_test "binary_to_int [1; 0] is 2" [ 1; 0 ] 2;
@@ -48,7 +43,122 @@ let one_tests =
        Dead; Dead |]"
       10
       [| Dead; Dead; Dead; Dead; Dead; Alive; Dead; Dead; Dead; Dead |];
-      
+  ]
+
+let int_to_binary_tests =
+  [
+    int_to_binary_test "int_to_binary 0 is [0]" 0 [ 0 ];
+    int_to_binary_test "int_to_binary 1 is [1]" 1 [ 1 ];
+    int_to_binary_test "int_to_binary 2 is [0; 1]" 2 [ 1; 0 ];
+    int_to_binary_test "int_to_binary 3 is [1; 1]" 3 [ 1; 1 ];
+    int_to_binary_test "int_to_binary 4 is [1; 0; 0]" 4 [ 1; 0; 0 ];
+    int_to_binary_test "int_to_binary 5 is [1; 0; 1]" 5 [ 1; 0; 1 ];
+    int_to_binary_test "int_to_binary 6 is [1; 1; 0]" 6 [ 1; 1; 0 ];
+    int_to_binary_test "int_to_binary 7 is [1; 1; 1]" 7 [ 1; 1; 1 ];
+    int_to_binary_test "int_to_binary 8 is [1; 0; 0; 0]" 8 [ 1; 0; 0; 0 ];
+    int_to_binary_test "int_to_binary 9 is [1; 0; 0; 1]" 9 [ 1; 0; 0; 1 ];
+    int_to_binary_test "int_to_binary 10 is [1; 0; 1; 0]" 10 [ 1; 0; 1; 0 ];
+    int_to_binary_test "int_to_binary 11 is [1; 0; 1; 1]" 11 [ 1; 0; 1; 1 ];
+    int_to_binary_test "int_to_binary 12 is [1; 1; 0; 0]" 12 [ 1; 1; 0; 0 ];
+    int_to_binary_test "int_to_binary 13 is [1; 1; 0; 1]" 13 [ 1; 1; 0; 1 ];
+    int_to_binary_test "int_to_binary 14 is [1; 1; 1; 0]" 14 [ 1; 1; 1; 0 ];
+    int_to_binary_test "int_to_binary 15 is [1; 1; 1; 1]" 15 [ 1; 1; 1; 1 ];
+    int_to_binary_test "int_to_binary 16 is [1; 0; 0; 0; 0]" 16
+      [ 1; 0; 0; 0; 0 ];
+    int_to_binary_test "int_to_binary 17 is [1; 0; 0; 0; 1]" 17
+      [ 1; 0; 0; 0; 1 ];
+    int_to_binary_test "int_to_binary 18 is [1; 0; 0; 1; 0]" 18
+      [ 1; 0; 0; 1; 0 ];
+    int_to_binary_test "int_to_binary 19 is [1; 0; 0; 1; 1]" 19
+      [ 1; 0; 0; 1; 1 ];
+    int_to_binary_test "int_to_binary 20 is [1; 0; 1; 0; 0]" 20
+      [ 1; 0; 1; 0; 0 ];
+    int_to_binary_test "int_to_binary 21 is [1; 0; 1; 0; 1]" 21
+      [ 1; 0; 1; 0; 1 ];
+    int_to_binary_test "int_to_binary 22 is [1; 0; 1; 1; 0]" 22
+      [ 1; 0; 1; 1; 0 ];
+    int_to_binary_test "int_to_binary 23 is [1; 0; 1; 1; 1]" 23
+      [ 1; 0; 1; 1; 1 ];
+    int_to_binary_test "int_to_binary 24 is [1; 1; 0; 0; 0]" 24
+      [ 1; 1; 0; 0; 0 ];
+    int_to_binary_test "int_to_binary 25 is [1; 1; 0; 0; 1]" 25
+      [ 1; 1; 0; 0; 1 ];
+    int_to_binary_test "int_to_binary 26 is [1; 1; 0; 1; 0]" 26
+      [ 1; 1; 0; 1; 0 ];
+    int_to_binary_test "int_to_binary 27 is [1; 1; 0; 1; 1]" 27
+      [ 1; 1; 0; 1; 1 ];
+    int_to_binary_test "int_to_binary 28 is [1; 1; 1; 0; 0]" 28
+      [ 1; 1; 1; 0; 0 ];
+    int_to_binary_test "int_to_binary 29 is [1; 1; 1; 0; 1]" 29
+      [ 1; 1; 1; 0; 1 ];
+    int_to_binary_test "int_to_binary 30 is [1; 1; 1; 1; 0]" 30
+      [ 1; 1; 1; 1; 0 ];
+    int_to_binary_test "int_to_binary 31 is [1; 1; 1; 1; 1]" 31
+      [ 1; 1; 1; 1; 1 ];
+    int_to_binary_test "int_to_binary 32 is [1; 0; 0; 0; 0; 0]" 32
+      [ 1; 0; 0; 0; 0; 0 ];
+    int_to_binary_test "int_to_binary 33 is [1; 0; 0; 0; 0; 1]" 33
+      [ 1; 0; 0; 0; 0; 1 ];
+    int_to_binary_test "int_to_binary 34 is [1; 0; 0; 0; 1; 0]" 34
+      [ 1; 0; 0; 0; 1; 0 ];
+    int_to_binary_test "int_to_binary 35 is [1; 0; 0; 0; 1; 1]" 35
+      [ 1; 0; 0; 0; 1; 1 ];
+    int_to_binary_test "int_to_binary 36 is [1; 0; 0; 1; 0; 0]" 36
+      [ 1; 0; 0; 1; 0; 0 ];
+    int_to_binary_test "int_to_binary 37 is [1; 0; 0; 1; 0; 1]" 37
+      [ 1; 0; 0; 1; 0; 1 ];
+    int_to_binary_test "int_to_binary 38 is [1; 0; 0; 1; 1; 0]" 38
+      [ 1; 0; 0; 1; 1; 0 ];
+    int_to_binary_test "int_to_binary 39 is [1; 0; 0; 1; 1; 1]" 39
+      [ 1; 0; 0; 1; 1; 1 ];
+    int_to_binary_test "int_to_binary 40 is [1; 0; 1; 0; 0; 0]" 40
+      [ 1; 0; 1; 0; 0; 0 ];
+    int_to_binary_test "int_to_binary 41 is [1; 0; 1; 0; 0; 1]" 41
+      [ 1; 0; 1; 0; 0; 1 ];
+    int_to_binary_test "int_to_binary 42 is [1; 0; 1; 0; 1; 0]" 42
+      [ 1; 0; 1; 0; 1; 0 ];
+    int_to_binary_test "int_to_binary 43 is [1; 0; 1; 0; 1; 1]" 43
+      [ 1; 0; 1; 0; 1; 1 ];
+    int_to_binary_test "int_to_binary 44 is [1; 0; 1; 1; 0; 0]" 44
+      [ 1; 0; 1; 1; 0; 0 ];
+    int_to_binary_test "int_to_binary 45 is [1; 0; 1; 1; 0; 1]" 45
+      [ 1; 0; 1; 1; 0; 1 ];
+    int_to_binary_test "int_to_binary 46 is [1; 0; 1; 1; 1; 0]" 46
+      [ 1; 0; 1; 1; 1; 0 ];
+    int_to_binary_test "int_to_binary 47 is [1; 0; 1; 1; 1; 1]" 47
+      [ 1; 0; 1; 1; 1; 1 ];
+    int_to_binary_test "int_to_binary 48 is [1; 1; 0; 0; 0; 0]" 48
+      [ 1; 1; 0; 0; 0; 0 ];
+    int_to_binary_test "int_to_binary 49 is [1; 1; 0; 0; 0; 1]" 49
+      [ 1; 1; 0; 0; 0; 1 ];
+    int_to_binary_test "int_to_binary 50 is [1; 1; 0; 0; 1; 0]" 50
+      [ 1; 1; 0; 0; 1; 0 ];
+    int_to_binary_test "int_to_binary 51 is [1; 1; 0; 0; 1; 1]" 51
+      [ 1; 1; 0; 0; 1; 1 ];
+    int_to_binary_test "int_to_binary 52 is [1; 1; 0; 1; 0; 0]" 52
+      [ 1; 1; 0; 1; 0; 0 ];
+    int_to_binary_test "int_to_binary 53 is [1; 1; 0; 1; 0; 1]" 53
+      [ 1; 1; 0; 1; 0; 1 ];
+    int_to_binary_test "int_to_binary 54 is [1; 1; 0; 1; 1; 0]" 54
+      [ 1; 1; 0; 1; 1; 0 ];
+    int_to_binary_test "int_to_binary 55 is [1; 1; 0; 1; 1; 1]" 55
+      [ 1; 1; 0; 1; 1; 1 ];
+    int_to_binary_test "int_to_binary 56 is [1; 1; 1; 0; 0; 0]" 56
+      [ 1; 1; 1; 0; 0; 0 ];
+    int_to_binary_test "int_to_binary 57 is [1; 1; 1; 0; 0; 1]" 57
+      [ 1; 1; 1; 0; 0; 1 ];
+    int_to_binary_test "int_to_binary 58 is [1; 1; 1; 0; 1; 0]" 58
+      [ 1; 1; 1; 0; 1; 0 ];
+    int_to_binary_test "int_to_binary 59 is [1; 1; 1; 0; 1; 1]" 59
+      [ 1; 1; 1; 0; 1; 1 ];
+    int_to_binary_test "int_to_binary 60 is [1; 1; 1; 1; 0; 0]" 60
+      [ 1; 1; 1; 1; 0; 0 ];
+    int_to_binary_test "int_to_binary 61 is [1; 1; 1; 1; 0; 1]" 61
+      [ 1; 1; 1; 1; 0; 1 ];
+    int_to_binary_test "int_to_binary 62 is [1; 1; 1; 1; 1; 0]" 62
+      [ 1; 1; 1; 1; 1; 0 ];
+    int_to_binary_test "int_to_binary 63 is [1; 1; 1; 1; 1; 1]" 63
+      [ 1; 1; 1; 1; 1; 1 ];
   ]
 
 (* Test Boards *)
@@ -190,5 +300,8 @@ let gol_tests = List.flatten [ neighbors_tests; update_node_tests ]
 
 (******************************************************************************)
 
-let suite = "test suite for CA" >::: List.flatten [ gol_tests; one_tests ]
+let suite =
+  "test suite for CA"
+  >::: List.flatten [ gol_tests; one_tests; int_to_binary_tests ]
+
 let _ = run_test_tt_main suite
